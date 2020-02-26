8e7882aa531f522e639609e707383ce93d1ea037
thalvari <tuomas.halvari@helsinki.fi>
2020-02-26 16:45:14 +0200

    small changes

--- countries.py
+++ countries.py
@@ -11,6 +11,7 @@ def get_country(nconst):
             birth_place = person.data["birth info"]["birth place"]
             country = birth_place.split(",")[-1].strip()
             country = country.split("now")[-1].strip()
+            country = country.split("in")[-1].strip()
             country = "".join([ch for ch in country if ch.isalpha() or ch in {" ", "-"}])
         print(country + "|" + birth_place)
     except:
